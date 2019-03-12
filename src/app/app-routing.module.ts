8ac433e703c3c997309034d8d8c6508d0981de6a
= <=>
2019-03-13 00:11:33 +0700

    vẽ giao diên thêm công việc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { AddTaskComponent } from './components/task-management/add-task/add-task.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'tasks',
+    component: AddTaskComponent
   }
 ];
 
