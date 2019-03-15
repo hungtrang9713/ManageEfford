7ec9a6ac5ad768838401133037a97ada4d057aef
= <=>
2019-03-15 23:56:11 +0700

    modeling list nhân viên và công việc

--- src/app/components/home/produce-point/add-task/add-task.component.ts
+++ src/app/components/home/produce-point/add-task/add-task.component.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+export interface Task {
+  PBI: number;
+  TaskName: string;
+  EffortPoint: number;
+  MinusPoint: number;
+  FinalScore: number;
+  Note: string;
+}
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.scss']
 })
+
 export class AddTaskComponent implements OnInit {
-  employeeID: number;
-  constructor(private route: ActivatedRoute) { }
+  // id hứng từ bản ghi
+  ID: number;
+  // tạo một mảng quản lý các task
+  tasks: Array<Task> = [];
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
 
   ngOnInit() {
-    this.employeeID = this.route.snapshot.params.id;
-    console.log(this.employeeID);
+    // fake du lieu
+    this.tasks = [{
+      PBI: 1997,
+      TaskName: `Đi học`,
+      EffortPoint: 4,
+      MinusPoint: 0,
+      FinalScore: 4,
+      Note: ``
+    }, {
+      PBI: 1997,
+      TaskName: `Đi ăn`,
+      EffortPoint: 4,
+      MinusPoint: 0,
+      FinalScore: 4,
+      Note: `chậm trể`
+    }];
   }
 
 }
