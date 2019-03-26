ceb56bf428b44cecdb8f7c5bf88411b6031163b2
= <=>
2019-03-27 00:24:15 +0700

    thêm giao diện thêm user

--- src/app/components/home/produce-point/list-developer/list-developer.component.ts
+++ src/app/components/home/produce-point/list-developer/list-developer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatTableDataSource, MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { CalendarTodayDirective } from 'angular-calendar/modules/common/calendar-today.directive';
 import { Month } from 'src/app/shared/array/moth';
@@ -7,6 +7,8 @@ import { Year } from 'src/app/shared/array/year';
 import { Subscription } from 'rxjs';
 import { TaskService } from 'src/app/shared/services/task/task.service';
 import { DataMonth } from 'src/app/shared/models/data-month';
+import { AddUserPopupComponent } from 'src/app/shared/components/add-user-popup/add-user-popup.component';
+import { NotifierService } from 'angular-notifier';
 
 /**
  * @title Table with pagination
@@ -46,55 +48,68 @@ export class ListDeveloperComponent implements OnInit, OnDestroy {
   public dataSource = new MatTableDataSource<DataMonth>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  constructor(private navigator: Router, private taskSV: TaskService) {
-  }
-  //#endregion property
+  constructor(private navigator: Router,
+    private taskSV: TaskService,
+    public addUserDialog: MatDialog,
+    public notifier: NotifierService
+  ) {}
 
-  //#region life cycle
-  ngOnInit() {
-    this.dataSource.paginator = this.paginator;
-    // gọi hàm getdataMoth
-    this.getDataMonth(this.monthSelected, this.yearSelected);
-  }
-  ngOnDestroy(): void {
-    this.subcription.forEach(sub => {
-      sub.unsubscribe();
-    });
-  }
-  //#endregion
+//#endregion property
 
-  //#region functions
-  getDataMonth(m, y) {
-    const dataMonthSub = this.taskSV.getDataMonth(m, y).subscribe(data => {
-      this.ELEMENT_DATA = data;
-      this.dataSource = new MatTableDataSource<DataMonth>(this.ELEMENT_DATA);
-    });
-    this.subcription.push(dataMonthSub);
-  }
+//#region life cycle
+ngOnInit() {
+  this.dataSource.paginator = this.paginator;
+  // gọi hàm getdataMoth
+  this.getDataMonth(this.monthSelected, this.yearSelected);
+}
+ngOnDestroy(): void {
+  this.subcription.forEach(sub => {
+    sub.unsubscribe();
+  });
+}
+//#endregion
 
-  /**
-   * hàm xử lý khi xem chi tiết nv
-   */
-  clickRow(row) {
-    this.navigator.navigate([`/home/produce-point/employee`, row.UserID, this.monthSelected, this.yearSelected]);
-  }
-  /**
-   * hàm lấy chi tiết nhân viên theo thánng
-   */
-  getDataByDate() {
-    // gọi api
-    this.getDataMonth(this.monthSelected, this.yearSelected);
-  }
-  /**
-   * thêm mới user
-   */
-  addEvent() {
-    //do smt
-  }
-  export(){
-    const config = `http://localhost:55465/Task/Export/3/2019/4E74DFFD-1188-46F2-BC42-E4746C5C39F1`;
-    window.open(config);
-  }
+//#region functions
+getDataMonth(m, y) {
+  const dataMonthSub = this.taskSV.getDataMonth(m, y).subscribe(data => {
+    this.ELEMENT_DATA = data;
+    this.dataSource = new MatTableDataSource<DataMonth>(this.ELEMENT_DATA);
+  });
+  this.subcription.push(dataMonthSub);
+}
+
+/**
+ * hàm xử lý khi xem chi tiết nv
+ */
+clickRow(row) {
+  this.navigator.navigate([`/home/produce-point/employee`, row.UserID, this.monthSelected, this.yearSelected]);
+}
+/**
+ * hàm lấy chi tiết nhân viên theo thánng
+ */
+getDataByDate() {
+  // gọi api
+  this.getDataMonth(this.monthSelected, this.yearSelected);
+}
+/**
+ * thêm mới user
+ */
+
+addUser() {
+  const dialogRef = this.addUserDialog.open(AddUserPopupComponent, {
+    data: {}
+  });
+  dialogRef.afterClosed().subscribe(result => {
+    // get lại dữ liệu
+    if (result) {
+      this.notifier.notify('success', 'Thêm thành công');
+    }
+  });
+}
+  export (){
+  const config = `http://localhost:55465/Task/Export/3/2019/CF577878-5B80-41C4-9901-B71B8B53C649`;
+  window.open(config);
+}
 }
 
 
